e97af4ea6287b84c62bae65ca8ec0c96f6f2d879
bnh5992 <95531379+bnh5992@users.noreply.github.com>
2023-05-11 13:02:27 +0900

    [Silver V] Title: 타일 장식물, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        int[] fibo = new int[n+1];
+        dp[0] = 4;
+        dp[1] = 6;
+        fibo[0] = 1;
+        fibo[1] = 1;
+
+
+        for (int i = 2; i < n; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+            dp[i] = dp[i-1] + 2*fibo[i];
+        }
+        System.out.println(dp[n-1]);
+    }
+}
